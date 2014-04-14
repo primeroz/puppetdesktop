e2fd613d185add30f6a489006e69b7d56d7dcdf8
Francesco Ciocchetti <francesco.ciocchetti@booking.com>
2014-04-14 11:50:38 +0100

    add base bash module

---
+++ modules/bash/manifests/init.pp
@@ -0,0 +1,41 @@
+class bash (
+    $bashd_enabled = true,
+    $user = undef,
+
+) {
+
+    if $user == undef {
+      fail("\$user need to be specified for bash module")
+    }
+
+    #TODO Use Templates and Profiles better
+    file { "bashrc_user":
+      source => "puppet:///modules/bash/bashrc-default",
+      path   => "/home/${user}/.bash.d/",
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+      require => Package['bash'],
+    }
+
+    file { "bashd_home_directory":
+      ensure => "directory"
+      path   => "/home/${user}/.bash.d/",
+      owner   => $user,
+      group   => $user,
+      mode    => '0750',
+      require => File['bashrc_user'],
+    }
+
+    file { "bashd_home_snippets":
+      source => "puppet:///modules/bash/bash.d/",
+      path   => "/home/${user}/.bash.d/",
+      recurse => true,
+      owner   => $user,
+      group   => $user,
+      mode    => '0640',
+      require => File['bashd_home_directory'],
+    }
+
+
+}
