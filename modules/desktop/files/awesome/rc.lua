d59f32ffb02ce993fe650cf5ab05c66bd8c43f34
Francesco Ciocchetti <francesco.ciocchetti@booking.com>
2014-04-14 11:22:48 +0100

    add theme and base modular config for awesome 3.4

---
+++ modules/desktop/files/awesome/rc.lua
@@ -0,0 +1,113 @@
+-- Standard awesome library
+require("awful")
+require("awful.autofocus")
+require("awful.rules")
+-- Theme handling library
+require("beautiful")
+-- Notification library
+require("naughty")
+
+-- Load Debian menu entries
+-- require("debian.menu")
+
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
+-- {{{   7 -- Simple function to load additional LUA files from rc/.
+function loadrc(name, mod)
+   local success
+   local result
+
+   -- Which file? In rc/ or in lib/?
+   local path = awful.util.getdir("config") .. "/" ..
+      (mod and "lib" or "rc") ..
+      "/" .. name .. ".lua"
+
+   -- If the module is already loaded, don't load it again
+   if mod and package.loaded[mod] then return package.loaded[mod] end
+
+   -- Execute the RC/module file
+   success, result = pcall(function() return dofile(path) end)
+   if not success then
+      naughty.notify({ title = "Error while loading an RC file",
+           text = "When loading `" .. name ..
+        "`, got the following error:\n" .. result,
+           preset = naughty.config.presets.critical
+         })
+      return print("E: error loading RC file '" .. name .. "': " .. result)
+   end
+
+   -- Is it a module?
+   if mod then
+      return package.loaded[mod]
+   end
+
+   return result
+end
+-- }}}
+
+
+-- {{{ Variable definitions
+-- Themes define colours, icons, and wallpapers
+-- beautiful.init("/usr/share/awesome/themes/sky/theme.lua")
+beautiful.init(awful.util.getdir("config") .. "/themes/grey-new/theme.lua")
+
+
+-- This is used later as the default terminal and editor to run.
+terminal = "x-terminal-emulator"
+editor = os.getenv("EDITOR") or "editor"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+modkey = "Mod4"
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+layouts =
+{
+    awful.layout.suit.floating,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.spiral,
+    awful.layout.suit.spiral.dwindle,
+    awful.layout.suit.max,
+    awful.layout.suit.max.fullscreen,
+    awful.layout.suit.magnifier
+}
+-- }}}
+
+loadrc("rc.tags")
+loadrc("rc.menu")
+loadrc("rc.widgets")
+loadrc("rc.bindings")
+loadrc("rc.rules")
+loadrc("rc.signals")
+
