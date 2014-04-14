03ffafa7e80e2748101cc15e05be2aeaba9cb28e
Francesco Ciocchetti <francesco.ciocchetti@booking.com>
2014-04-14 11:07:55 +0100

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if not [ "$(id -u)" -eq 0 ]; then
+if ! [ "$(id -u)" -eq 0 ]; then
     echo "execute me as root -.-" >&2
     exit 1
 else
     basedir="$(dirname $0)"
-    puppet apply --modulepath "${basedir}/modules" "${basedir}/run.pp" "$@"
+    puppet apply --modulepath "${basedir}/modules" --hiera_config "${basedir}/hiera.yaml" "${basedir}/manifests/default.pp" "$@"
 fi
 
